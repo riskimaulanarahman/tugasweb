c53e855b9324d5efcdb8392ec523a9fdbac51b74
riskimaulanarahman <61986465+riskimaulanarahman@users.noreply.github.com>
2021-09-23 17:21:32 +0800

    Update proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -1,26 +1,22 @@
 <?php
-session_start();
+
 include "koneksi.php";
 $username = $_POST['username'];
 $pass = ($_POST['password']);
 $login = mysqli_query($koneksi,"SELECT * FROM user WHERE username='$username' and password='$pass'");
 $cek = mysqli_num_rows($login);
- 
+$row_akun = mysqli_fetch_array($login);
+
 if ($cek > 0) {
-  
-  $_SESSION['status'] = "login";
-  $row_akun = mysqli_fetch_array($sql_login);
-            $_SESSION["id_user"] = $row_akun["id_user"];
-            $_SESSION["nik"] = $row_akun["nik"];
-            $_SESSION["nama"] = $row_akun["nama"];
-            $_SESSION["username"] = $row_akun["username"];
-            $_SESSION["password"] = $row_akun["password"];
-			$_SESSION["telp"] = $row_akun["telp"];
- sumber: https://www.posciety.com/cara-menampilkan-data-dari-database-berbeda-setiap-akun/
+  session_start();
+    $_SESSION['status'] = "login";
+    $_SESSION["nik"] = $row_akun["nik"];
+    $_SESSION["nama"] = $row_akun["nama"];
+    $_SESSION["username"] = $row_akun["username"];
   header("location:index.php");
  
 }else {
   header("location:login.php?pesan=gagal");
  
 }
- ?>
+?>
