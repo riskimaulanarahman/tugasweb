87e0a0dad1e7eb8bd817eec02f15cd2f8e211a2a
rishnaskr <rishnaskr@gmail.com>
2021-09-23 16:13:25 +0800

    login

--- pengaduan_masyarakat.sql
+++ pengaduan_masyarakat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Sep 2021 pada 03.26
--- Versi server: 10.4.20-MariaDB
--- Versi PHP: 8.0.9
+-- Waktu pembuatan: 23 Sep 2021 pada 10.12
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,45 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `pengaduan_masyarakat`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `masyarakat`
---
-
-CREATE TABLE `masyarakat` (
-  `id_masyarakat` int(20) NOT NULL,
-  `nik` int(11) NOT NULL,
-  `nama` varchar(35) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `telp` varchar(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `masyarakat`
---
-
-INSERT INTO `masyarakat` (`id_masyarakat`, `nik`, `nama`, `username`, `password`, `telp`) VALUES
-(1, 52666, 'aigj', '0', '123', '1234'),
-(2, 0, '', '0', '', ''),
-(3, 0, '', '0', '', ''),
-(4, 0, '', '0', '', ''),
-(5, 0, '', '0', '', ''),
-(6, 0, '', '0', '', ''),
-(7, 0, '', '0', '', ''),
-(8, 0, '', '0', '', ''),
-(9, 0, '', '0', '', ''),
-(10, 0, '', '0', '', ''),
-(11, 4567890, 'jshg', '0', '123', '567890'),
-(12, 4567890, 'jshg', 'galur', '123', '1234'),
-(13, 4567890, 'hkl', 'Dafa', 'nm', '90'),
-(14, 4223, 'fafa', 'galur', '123', 'faf'),
-(15, 684849, 'jshg', 'galur', 'e', '21'),
-(16, 684849, 'aigj', 'galur', 'dD', '7567'),
-(17, 684849, 'jshg', 'galur', 'Wdm4MZQ9F2TBW65', '890'),
-(18, 123456, 'hfhjfjh', 'galur', 'Wdm4MZQ', '456789');
 
 -- --------------------------------------------------------
 
@@ -86,7 +47,7 @@ CREATE TABLE `petugas` (
   `nama_petugas` varchar(35) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `tel` varchar(13) NOT NULL,
+  `telp` varchar(13) NOT NULL,
   `level` enum('admin','petugas') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -104,15 +65,34 @@ CREATE TABLE `tanggapan` (
   `id_petugas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(20) NOT NULL,
+  `nik` char(16) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `level` enum('masyarakat','petugas','admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `nik`, `nama`, `username`, `password`, `telp`, `level`) VALUES
+(24, '6471025201950003', 'RISHNA SEKARNINGSIH', 'rishnaskr', '123', '082138634000', 'masyarakat'),
+(25, '6471042208950002', 'seto aji pratama', 'user10102020', '123', '082138634000', 'masyarakat'),
+(26, '6471044608960003', 'prasetyo/aries', 'rishnaskr123', '123', '082138634000', 'admin');
+
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `masyarakat`
---
-ALTER TABLE `masyarakat`
-  ADD PRIMARY KEY (`id_masyarakat`);
 
 --
 -- Indeks untuk tabel `pengaduan`
@@ -133,14 +113,14 @@ ALTER TABLE `tanggapan`
   ADD PRIMARY KEY (`id_tanggapan`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indeks untuk tabel `user`
 --
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel `masyarakat`
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
-ALTER TABLE `masyarakat`
-  MODIFY `id_masyarakat` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengaduan`
@@ -159,6 +139,12 @@ ALTER TABLE `petugas`
 --
 ALTER TABLE `tanggapan`
   MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
